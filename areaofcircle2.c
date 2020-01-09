d48e4bbbd57e37b2d6b7610123270955da571950
Christopher Phan <phanc2069@my.sduhsd.net>
2020-01-09 13:13:52 -0800

    work at home

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
 
-int main
+float ca(int i)
 {
+  float a;
+  a = i*i*3.14;
+  return a;
+}
+
+
+int main(float argc, char* argv[])
+{
+
+
+     float min = scanf(argv[1]);
+     float max = scanf(argv[2]);
+     float g;
+
+     if (min || max == 0);
+     {
+       for (float i = min; i < max; i++)
+       {
+            g = ca(i);
+            printf("Area of radius %f is %f\n", i, g);
+       }
+     }
+     int arg = 0;
+     while (arg < argc)
+     {
+          printf("Arg %d is %s\n", arg, argv[arg]);
+          arg++;
+     }
+}
