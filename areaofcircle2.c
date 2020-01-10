5d6e9d1699fc9f1560e127636263a24c60539ff5
phanc2069 <58751173+phanc2069@users.noreply.github.com>
2020-01-10 12:30:48 -0800

    Update areaofcircle2.c

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 
-float ca(int i)
+float circle_area(float i)
 {
   float a;
   a = i*i*3.14;
   return a;
 }
 
-
-int main(float argc, char* argv[])
+int main(int argc, char* argv[])
 {
-
-
-     float min = scanf(argv[1]);
-     float max = scanf(argv[2]);
-     float g;
-
-     if (min || max == 0);
+  int count = 0;
+  float min = 0, max = 0, g;
+  if (argc > 2)
+  {
+     count += sscanf(argv[1], "%f", &min);
+     count += sscanf(argv[2], "%f", &max);
+  }
+    while (count < 2)
+    {
+      printf("\nPlease enter two numbers greater than zero: ");
+      count = scanf("%f %f", &min, &max);
+      if (count < 2)
+      {
+        char junk[100];
+        scanf("%s", junk);
+      }
+      else if (max <= min)
+      {
+        printf("Min must be smaller than max.\n");
+        count = 0;
+      }
+      
+    }
+     if (min || max >= 0)
      {
-       for (float i = min; i < max; i++)
+       for (float i = min; i <= max; ++i)
        {
-            g = ca(i);
-            printf("Area of radius %f is %f\n", i, g);
+            // printf("get here 2");
+            g = circle_area(i);
+            printf("Area of circle %f is %f\n", i, g);
        }
-     }
-     int arg = 0;
-     while (arg < argc)
-     {
-          printf("Arg %d is %s\n", arg, argv[arg]);
-          arg++;
      }
 }
