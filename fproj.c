e8e2dfad8691555bec468887cf873dc1d6713ed1
phanc2069 <58751173+phanc2069@users.noreply.github.com>
2020-01-15 22:44:43 -0800

    Update fproj.c

--- fproj.c
+++ fproj.c
@@ -2,36 +2,77 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-float pascaltriangle (float a) {
-    int rows, coef=1, space, i, j;
-    rows = scanf("%d", &rows);
-    for (i=0; i<rows; i++) {
-        for (space=1; space <= rows-i; space++)
-            printf("  ");
-        for (j=0; j<=i; j++) {
-            if (j==0 || i==0)
-                coef = 1;
-            else
-                coef=coef*(i-j+1)/j;
-            printf("%4d", coef);
-        }
-        printf("\n");
-    }
-    return i;
-}
-
 int main (int argc, char *argv[]) {
- char input[1028], name [1028];
+ char input[1028];
  float age;
  int arg = 0;
- float r;
  int c = 0;
- float csize;
- int g;
-
-  r = atof(argv[arg+1]);
-  printf("%f\n", r);
-  csize = pascaltriangle(r);
-  printf("Pascal Triangle with %d rows is %f\n", arg+1, csize);
-  arg++;
+ int g, y, z;
+ int rows, coef=1, space, i, j;
+ if(atoi(argv[1])>=atoi(argv[2])) {
+   if(isdigit(*argv[1])) {
+     rows = atoi(argv[1]);
+     for (i=0; i<rows; i++) {
+       for (space=1; space <= rows-i; space++)
+           printf("  ");
+       for (j=0; j<=i; j++) {
+         if (j==0 || i==0)
+             coef = 1;
+         else
+             coef=coef*(i-j+1)/j;
+         printf("%4d", coef);
+       }
+       printf("\n");
+    }
+  }
+    if(argv[2] == 0) {
+    for(i=(rows-1); i<rows; i++) {
+     for (space=1; space <= rows-i; space++) {
+           printf("  ");
+      }     
+      for (j=0; j<=i; j++) {
+       if (j==0 || i==0) {
+          coef = 1;
+       }   
+         else {
+             coef=coef*(i-j+1)/j;
+             printf("%4d", coef);
+         }    
+     }
+     printf("\n");
+  }
+  return 0;
+ }
+  if(isdigit(*argv[2])) {
+  if(atoi(argv[2]) != 0) {
+   for(i=(rows-1); i<rows; i++) {
+        for (j=(atoi(argv[2]+3)); j<atoi(argv[2]); j++) {
+          if (j==0 || i==0) {
+              coef = 1;
+          }   
+          else {
+              coef=coef*(i-j+1)/j;
+          }
+        }
+      printf("\nIn row %d, the value you seek is %d.", rows, coef);
+      printf("\n");
+     }
+    }
+    else {
+      for(i=(rows-1); i<rows; i++) {
+      for (space=1; space <= rows-i; space++)
+            printf("  ");
+        for (j=0; j<=i; j++) {
+          if (j==0 || i==0)
+              coef = 1;
+          else
+              coef=coef*(i-j+1)/j;
+              printf("%4d", coef);
+      }
+      printf("\nHere is row %d", rows);
+      printf("\n");
+    }
+   }
+  }
+ }
 }
